85d97009f532e6c54083a599dc71d6b2e13b0245
David Masover <dave@3mix.com>
2009-03-05 21:08:36 -0600

    Added add_test_expressions. Spec passes.

--- lib/re_template.rb
+++ lib/re_template.rb
@@ -1,5 +1,19 @@
 class ReTemplate
-  attr_accessor :expressions, :nodes
+  attr_accessor :nodes
+  attr_writer :expressions
+  def expressions
+    @expressions ||= {}
+  end
+  
+  def add_text_expressions *args
+    hash = args.last.kind_of?(Hash) ? args.pop : {}
+    args.each do |field|
+      hash[field] = field
+    end
+    hash.each_pair do |key, value|
+      self.expressions[/#{Regexp.escape key}/] = value
+    end
+  end
   
   class ReTemplate::Text < ReTemplate
     def parse! string
